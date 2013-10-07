f507c59512f6e2badef1ab7d9b73663e53261bf5
Oleg Mihailik <mihailik@gmail.com>
2013-10-07 18:56:39 +0100

    Fixes for mscorlib.ts, full tracing in the browserwindow.

--- src/tool/tool2.js
+++ src/tool/tool2.js
@@ -1,4 +1,119 @@
+var pe;
+(function (pe) {
+    (function (tool) {
+        /**
+        * loading embedded mscorlib.dll into a [typed] array of 32-bit integers
+        */
+        function loadMscorlib(callback, restIntervalMs, log) {
+            var pieceCount = 16;
+            var i = 0;
+            var base64 = null;
+            var binText = null;
+            var buf = null;
+
+            setTimeout(nextGetBase64, restIntervalMs);
+
+            function nextGetBase64() {
+                try  {
+                    log('nextGetBase64:' + i + '...');
+                    base64 = getBase64(i);
+                    setTimeout(nextGetBinText, restIntervalMs);
+                } catch (error) {
+                    callback(null, error);
+                }
+            }
+
+            function nextGetBinText() {
+                try  {
+                    log('nextGetBinText:' + i + '...');
+                    binText = getBinText(base64);
+                    if (buf === null)
+                        buf = createBuffer(binText.length * pieceCount);
+
+                    setTimeout(nextCopyBinTextToBuffer, restIntervalMs);
+                } catch (error) {
+                    callback(null, error);
+                }
+            }
+
+            function nextCopyBinTextToBuffer() {
+                try  {
+                    log('nextCopyBinTextToBuffer:' + i + '...');
+                    var pieceLength = buf.length / pieceCount;
+                    copyBinTextToBuffer(binText, buf, i * pieceLength, pieceLength);
+                    i++;
+
+                    if (i < pieceCount) {
+                        setTimeout(nextGetBase64, restIntervalMs);
+                    } else {
+                        log('mscorlib.dll[' + buf.length + '/' + pieceCount + '] completed');
+                        callback(buf, null);
+                    }
+                } catch (error) {
+                    callback(null, error);
+                }
+            }
+        }
+        tool.loadMscorlib = loadMscorlib;
+
+        function getBase64(i) {
+            var mscorlibDllScript = document.getElementById('mscorlib' + i + '.dll');
+            if (mscorlibDllScript === null) {
+                return null;
+            }
+
+            return mscorlibDllScript.innerHTML || mscorlibDllScript.textContent;
+        }
+
+        function getBinText(base64) {
+            if (!_atob)
+                createAtob();
+            return _atob(base64);
+        }
+
+        function createBuffer(int32Length) {
+            if ('Uint32Array' in window)
+                return new Uint32Array(int32Length);
+            else
+                return [];
+        }
+
+        function copyBinTextToBuffer(binText, buf, bufOffset, bufCount) {
+            for (var i = bufOffset; i < bufCount; i++) {
+                buf[i] = binText.charCodeAt(i / 4) | binText.charCodeAt(i / 4 + 1) << 8 | binText.charCodeAt(i / 4 + 2) << 16 | binText.charCodeAt(i / 4 + 3) << 24;
+            }
+        }
+
+        var _atob;
+        function createAtob() {
+            if (window.atob && false) {
+                _atob = window.atob;
+            } else {
+                var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=', INVALID_CHARACTER_ERR = ((function () {
+                    try  {
+                        document.createElement('$');
+                    } catch (error) {
+                        return error;
+                    }
+                })());
+                _atob = function (input) {
+                    input = input.replace(/=+$/, '');
+                    if (input.length % 4 == 1)
+                        throw INVALID_CHARACTER_ERR;
+                    for (// initialize result and counters
+                    var bc = 0, bs, buffer, idx = 0, output = ''; buffer = input.charAt(idx++); ~buffer && (bs = bc % 4 ? (bs * 64) + buffer : buffer, (bc++ % 4)) ? output += String.fromCharCode(255 & bs >> (-2 * bc & 6)) : 0) {
+                        // try to find character in table (0-63, not found => -1)
+                        buffer = chars.indexOf(buffer);
+                    }
+                    return output;
+                };
+            }
+        }
+    })(pe.tool || (pe.tool = {}));
+    var tool = pe.tool;
+})(pe || (pe = {}));
 /// <reference path='../pe2.d.ts' />
+/// <reference path='mscorlib.ts' />
 
 
 var loadedFiles = {};
@@ -14,129 +129,27 @@ function init() {
     try  {
         var mscStart = getTime();
         var pageLoadTime = (mscStart - startPageLoading) / 1000;
-        var preLog = 'page ' + pageLoadTime + 's.';
-        log(preLog);
+        log('page ' + pageLoadTime + 's.');
 
-        var buf = null;
-        var pieceCount = 16;
-        var i = 0;
-        var base64;
-        var binText;
-
-        function nextGetBase64() {
-            try  {
-                log(preLog + ' nextGetBase64:' + i + '...');
-                base64 = getBase64(i);
-                setTimeout(nextGetBinText, 50);
-            } catch (error) {
-                alert(error + ' ' + error.message);
+        pe.tool.loadMscorlib(function (buffer, error) {
+            var mscTime = (getTime() - mscStart) / 1000;
+            if (error) {
+                log(error + ' ' + error.message + ' ' + mscTime + 's.');
+            } else {
+                log('mscorlib[' + buffer.length + '] ' + buffer[0].toString(16).toUpperCase() + 'h ' + mscTime + 's.');
             }
-        }
-
-        function nextGetBinText() {
-            try  {
-                log(preLog + ' nextGetBinText:' + i + '...');
-                binText = getBinText(base64);
-                if (buf === null)
-                    buf = createBuffer(binText.length * pieceCount);
-
-                setTimeout(nextCopyBinTextToBuffer, 50);
-            } catch (error) {
-                alert(error + ' ' + error.message);
-            }
-        }
-
-        function nextCopyBinTextToBuffer() {
-            try  {
-                log(preLog + ' nextCopyBinTextToBuffer:' + i + '...');
-                var pieceLength = buf.length / pieceCount;
-                copyBinTextToBuffer(binText, buf, i * pieceLength, pieceLength);
-                i++;
-
-                if (i < pieceCount) {
-                    setTimeout(nextGetBase64, 50);
-                } else {
-                    var mscTime = (getTime() - mscStart) / 1000;
-                    log(preLog + ' mscorlib.dll[' + buf.length + '/' + pieceCount + '] ' + mscTime + 's.');
-                }
-            } catch (error) {
-                alert(error + ' ' + error.message);
-            }
-        }
-
-        setTimeout(nextGetBase64, 50);
+        }, 25, log);
     } catch (error) {
         alert(error + ' ' + error.message);
     }
 
-    var logElement;
     function log(txt) {
-        if (!logElement) {
-            logElement = document.createElement('div');
-            document.body.appendChild(logElement);
-        }
-
-        //var current = logElement.textContent || logElement.innerText;
-        var newText = /*current +*/ txt;
-        logElement.textContent = newText;
-        logElement.innerText = newText;
-    }
-    log.toString = function () {
-        return logElement.textContent || logElement.innerText;
-    };
-}
-
-function getBase64(i) {
-    var mscorlibDllScript = document.getElementById('mscorlib' + i + '.dll');
-    if (mscorlibDllScript === null) {
-        return null;
-    }
-
-    return mscorlibDllScript.innerHTML || mscorlibDllScript.textContent;
-}
-
-function getBinText(base64) {
-    if (!_atob)
-        createAtob();
-    return _atob(base64);
-}
-
-function createBuffer(int32Length) {
-    if ('Uint32Array' in window)
-        return new Uint32Array(int32Length);
-    else
-        return [];
-}
-
-function copyBinTextToBuffer(binText, buf, bufOffset, bufCount) {
-    for (var i = bufOffset; i < bufCount; i++) {
-        buf[i] = binText.charCodeAt(i / 4) | binText.charCodeAt(i / 4 + 1) << 8 | binText.charCodeAt(i / 4 + 2) << 16 | binText.charCodeAt(i / 4 + 3) << 24;
-    }
-}
-
-var _atob;
-function createAtob() {
-    if (window.atob && false) {
-        _atob = window.atob;
-    } else {
-        var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=', INVALID_CHARACTER_ERR = ((function () {
-            try  {
-                document.createElement('$');
-            } catch (error) {
-                return error;
-            }
-        })());
-        _atob = function (input) {
-            input = input.replace(/=+$/, '');
-            if (input.length % 4 == 1)
-                throw INVALID_CHARACTER_ERR;
-            for (// initialize result and counters
-            var bc = 0, bs, buffer, idx = 0, output = ''; buffer = input.charAt(idx++); ~buffer && (bs = bc % 4 ? (bs * 64) + buffer : buffer, (bc++ % 4)) ? output += String.fromCharCode(255 & bs >> (-2 * bc & 6)) : 0) {
-                // try to find character in table (0-63, not found => -1)
-                buffer = chars.indexOf(buffer);
-            }
-            return output;
-        };
+        var logElement = document.createElement('div');
+        if ('textContent' in logElement)
+            logElement.textContent = txt;
+        else if ('innerText' in logElement)
+            logElement.innerText = txt;
+        document.body.appendChild(logElement);
     }
 }
 
--- src/tool/tool2.ts
+++ src/tool/tool2.ts
@@ -1,4 +1,5 @@
 /// <reference path='../pe2.d.ts' />
+/// <reference path='mscorlib.ts' />
 
 declare var startPageLoading: number;
 
@@ -15,149 +16,34 @@ function init() {
   try {
     var mscStart = getTime();
     var pageLoadTime = (mscStart - startPageLoading)/1000;
-    var preLog = 'page '+pageLoadTime+'s.';
-    log(preLog);
+    log('page '+pageLoadTime+'s.');
 
-    var buf: Uint32Array = null;
-    var pieceCount = 16;
-    var i = 0;
-    var base64: string;
-    var binText: string;
-    
-    function nextGetBase64() {
-      try {
-        log(preLog+' nextGetBase64:'+i+'...');
-        base64 = getBase64(i);
-        setTimeout(nextGetBinText, 50);
-      }
-      catch (error) {
-        alert(error+' '+error.message);
-      }
-    }
-
-    function nextGetBinText() {
-      try {
-        log(preLog+' nextGetBinText:'+i+'...');
-        binText = getBinText(base64);
-        if (buf===null)
-          buf = createBuffer(binText.length*pieceCount);
-  
-        setTimeout(nextCopyBinTextToBuffer, 50);
-      }
-      catch (error) {
-        alert(error+' '+error.message);
-      }
-    }
-
-    function nextCopyBinTextToBuffer() {
-      try {
-        log(preLog+' nextCopyBinTextToBuffer:'+i+'...');
-        var pieceLength = buf.length/pieceCount;
-        copyBinTextToBuffer(binText, buf, i*pieceLength, pieceLength);
-        i++;
-  
-        if (i<pieceCount) {
-          setTimeout(nextGetBase64, 50);
+    pe.tool.loadMscorlib(
+      (buffer: Uint32Array, error: Error) => {
+        var mscTime = (getTime() - mscStart)/1000;
+        if (error) {
+          log(error+' '+error.message+' '+mscTime+'s.');
         }
         else {
-          var mscTime = (getTime() - mscStart)/1000;
-          log(preLog+' mscorlib.dll['+buf.length+'/'+pieceCount+'] '+mscTime+'s.');
+          log('mscorlib['+buffer.length+'] '+buffer[0].toString(16).toUpperCase()+'h '+mscTime+'s.');
         }
-      }
-      catch (error) {
-        alert(error+' '+error.message);
-      }
-    }
-    
-    setTimeout(nextGetBase64, 50);
+      },
+      25,
+      log);
   }
   catch (error) {
     alert(error+' '+error.message);
   }
 
-  var logElement;
   function log(txt) {
-    if (!logElement) {
-      logElement = document.createElement('div');
-      document.body.appendChild(logElement);
-    }
-    //var current = logElement.textContent || logElement.innerText;
-    var newText = /*current +*/ txt;
-    logElement.textContent = newText;
-    logElement.innerText = newText;
-  }
-  log.toString = function() {
-    return logElement.textContent || logElement.innerText;
+    var logElement = document.createElement('div');
+    if ('textContent' in logElement)
+      logElement.textContent = txt;
+    else if ('innerText' in logElement)
+      logElement.innerText = txt;
+    document.body.appendChild(logElement);
   }
 }
 
-function getBase64(i: number) {
-  var mscorlibDllScript = document.getElementById('mscorlib'+i+'.dll');
-  if (mscorlibDllScript===null) {
-    return null;
-  }
-
-  return mscorlibDllScript.innerHTML || mscorlibDllScript.textContent;
-}
-
-function getBinText(base64: string) {
-  if (!_atob)
-    createAtob();
-  return _atob(base64);
-}
-
-function createBuffer(int32Length: number): Uint32Array {
-  if ('Uint32Array' in window)
-    return new Uint32Array(int32Length);
-  else
-    return <any>[];
-}
-
-function copyBinTextToBuffer(
-  binText: string,
-  buf: Uint32Array,
-  bufOffset: number, bufCount: number) {
-
-  for (var i = bufOffset; i < bufCount; i++) {
-    buf[i] =
-      binText.charCodeAt(i/4) |
-      binText.charCodeAt(i/4 + 1) << 8 |
-      binText.charCodeAt(i/4 + 2) << 16 |
-      binText.charCodeAt(i/4 + 3) << 24;
-  }
-}
-
-var _atob;
-function createAtob() {
-  if (window.atob && false) {
-    _atob = window.atob;
-  }
-  else {
-    var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=',
-      INVALID_CHARACTER_ERR = (function () {
-        // fabricate a suitable error object
-        try { document.createElement('$'); }
-        catch (error) { return error; }}());
-    _atob = function (input) {
-      input = input.replace(/=+$/, '')
-      if (input.length % 4 == 1) throw INVALID_CHARACTER_ERR;
-      for (
-        // initialize result and counters
-        var bc: any = 0, bs: any, buffer: any, idx: any = 0, output: any = '';
-        // get next character
-        buffer = input.charAt(idx++);
-        // character found in table? initialize bit storage and add its ascii value;
-        ~buffer && (bs = bc % 4 ? <any>(bs * 64) + buffer : buffer,
-          // and if not first of each 4 characters,
-          // convert the first 8 bits to one ascii character
-          <any>(bc++ % 4)) ? output += <any>String.fromCharCode(255 & bs >> (-2 * bc & 6)) : <any>0
-      ) {
-        // try to find character in table (0-63, not found => -1)
-        buffer = chars.indexOf(buffer);
-      }
-      return output;
-    };
-  }
-}
 
 window.onload = init;
